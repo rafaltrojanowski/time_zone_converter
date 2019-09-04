c591f30e195d8d6b3a38be148808d67bde81ece1
Rafał Trojanowski <rt.trojanowski@gmail.com>
2019-09-04 14:08:24 +0700

    Add initial implementation

---
+++ lib/time_zone_converter.rb
@@ -0,0 +1,6 @@
+require "time_zone_converter/version"
+
+module TimeZoneConverter
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ spec/time_zone_converter_spec.rb
@@ -0,0 +1,9 @@
+RSpec.describe TimeZoneConverter do
+  it "has a version number" do
+    expect(TimeZoneConverter::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
