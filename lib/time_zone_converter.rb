27d412f8fa2f835f70d4e1f8c4f4445586d7fec7
Rafał Trojanowski <rt.trojanowski@gmail.com>
2019-09-05 12:16:12 +0700

    Add option and command to specify time

--- lib/time_zone_converter.rb
+++ lib/time_zone_converter.rb
@@ -10,13 +10,14 @@ module TimeZoneConverter
   # Inspired by:
   # https://stackoverflow.com/questions/8349817/ruby-gem-for-finding-timezone-of-location
 
-  def self.call(args)
-    args.map { |city| [city, get_time(city)] }
+  def self.call(args, time = Time.current)
+    time = get_time_from_string(time) if time.is_a? String
+    args.map { |city| [city, get_time(city, time)] }
   end
 
   private
 
-    def self.get_time(city, time = Time.now)
+    def self.get_time(city, time)
       json_file = 'data/cities.json'
       json = JSON.parse(File.read(json_file))
       item = json.select { |k, _| k == city }
@@ -29,4 +30,21 @@ module TimeZoneConverter
       time_zone = NearestTimeZone.to(lat.to_f, lng.to_f)
       time = time.in_time_zone(time_zone)
     end
+
+    def self.get_time_from_string(time)
+      hh = time.split(":").first.to_i
+      mm = time.split(":").last.to_i
+      offset = 0
+
+      Time.new(
+        Time.current.year,
+        Time.current.month,
+        Time.current.day,
+        hh,
+        mm,
+        0,
+        offset
+      )
+    end
+
 end
--- lib/time_zone_converter/cli.rb
+++ lib/time_zone_converter/cli.rb
@@ -11,5 +11,15 @@ module TimeZoneConverter
     def c(*args)
       puts TimeZoneConverter.call(args).inspect
     end
+
+    desc 'ct',
+      "
+        TODO:
+      "
+    def ct(*args)
+      cities = args.shift(args.size - 1)
+      time = args.first
+      puts TimeZoneConverter.call(cities, time).inspect
+    end
   end
 end
