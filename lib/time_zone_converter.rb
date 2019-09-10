f313edeb278f83b6898184c53094202f5b242707
Rafał Trojanowski <rt.trojanowski@gmail.com>
2019-09-10 14:12:03 +0700

    Add new command, it calculates local time in another cities

--- lib/time_zone_converter.rb
+++ lib/time_zone_converter.rb
@@ -12,8 +12,12 @@ module TimeZoneConverter
   # Inspired by:
   # https://stackoverflow.com/questions/8349817/ruby-gem-for-finding-timezone-of-location
 
-  def self.call(args, time = Time.current)
-    time = string_to_time(time) if time.is_a? String
+  def self.call(args, time = Time.current, method)
+    if method == :local
+      time_zone = get_zone(args.first)
+    end
+
+    time = string_to_time(time, method, time_zone) if time.is_a? String
     args.map { |city| [city, get_time(city, time)] }
   end
 
@@ -33,25 +37,54 @@ module TimeZoneConverter
       time = time.in_time_zone(time_zone)
     end
 
+    def self.get_zone(city)
+      json_file = 'data/cities.json'
+      json = Oj.load(File.read(json_file))
+      item = json.select! { |k, _| k == city }
+
+      raise "Not found #{city}" unless item
+
+      lat = item[city][0]
+      lng = item[city][1]
+
+      time_zone = NearestTimeZone.to(lat.to_f, lng.to_f)
+    end
+
     ISO_TIME = /\A(\d\d):(\d\d)\z/
 
     # returns UTC
-    def self.string_to_time(string)
+    def self.string_to_time(string, method, time_zone)
       # https://github.com/rails/rails/blob/aeba121a83965d242ed6d7fd46e9c166079a3230/activemodel/lib/active_model/type/helpers/time_value.rb#L65
 
       if string =~ ISO_TIME
-        offset = 0 # UTC +0
-        current_time = Time.new.utc
 
-        Time.new(
-          current_time.year,
-          current_time.month,
-          current_time.day,
-          $1.to_i,
-          $2.to_i,
-          0,
-          offset
-        )
+        if method == :utc
+          offset = 0 # UTC +0
+          current_time = Time.new.utc
+
+          Time.new(
+            current_time.year,
+            current_time.month,
+            current_time.day,
+            $1.to_i,
+            $2.to_i,
+            0,
+            offset
+          )
+        else
+          current_time = Time.new
+          offset = Time.zone_offset(time_zone)
+
+          Time.new(
+            current_time.year,
+            current_time.month,
+            current_time.day,
+            $1.to_i,
+            $2.to_i,
+            0,
+            offset
+          )
+        end
       end
     end
 end
--- lib/time_zone_converter/cli.rb
+++ lib/time_zone_converter/cli.rb
@@ -3,18 +3,40 @@ require "thor"
 module TimeZoneConverter
   class Cli < Thor
 
-    desc 'c',
+    desc 'ct',
       "
-        TODO:
-      "
+        It allows to answer the question:
+        What time it is in x, y, z when in x there is HH:MM.
 
-    def c(*args)
-      puts TimeZoneConverter.call(args).inspect
+        Takes list of the cities and time (format: 'HH:MM' 24-hour).
+        Time zone is taken from the first city in the list.
+
+        Returns array of arrays: [[city, time], [...]]
+
+        Example: time_zone_converter ct 'Chiang Mai' Skopje '19:00'
+        =>
+          [
+            ['Chiang Mai', Tue, 10 Sep 2019 19:00:00 +07 +07:00],
+            ['Skopje, Tue, 10 Sep 2019 14:00:00 CEST +02:00]
+          ]
+      "
+    def ct(*args, method: :local)
+      cities = args.shift(args.size - 1)
+      time = args.first
+      puts TimeZoneConverter.call(cities, time, method).inspect
     end
 
     desc 'ctu',
       "
-        Takes cities list and time (in UTC), and return array of [city, time (with zone)]
+        Similar as above (ct), with the exception that time is in UTC +0
+
+        It allows to answer the question:
+        What time it is in x, y, z when UTC time is HH:MM.
+
+        Takes cities list and time (in UTC)
+
+        Returns array of arrays: [[city, time], [...]]
+
         Example: time_zone_converter ctu Bangkok Warszawa '10:00'
         => [
              ['Bangkok', Tue, 10 Sep 2019 17:00:00 +07 +07:00],
@@ -22,10 +44,10 @@ module TimeZoneConverter
            ]
 
       "
-    def ctu(*args)
+    def ctu(*args, method: :utc)
       cities = args.shift(args.size - 1)
       time = args.first
-      puts TimeZoneConverter.call(cities, time).inspect
+      puts TimeZoneConverter.call(cities, time, method).inspect
     end
   end
 end
