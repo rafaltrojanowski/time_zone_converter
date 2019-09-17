a5cd85e1e6344b8b768fb4f74f185abf65262b04
Rafał Trojanowski <rt.trojanowski@gmail.com>
2019-09-17 22:32:49 +0700

    Do not override keys when not necessary
    Closes #2

--- lib/time_zone_converter/json_data_transformer.rb
+++ lib/time_zone_converter/json_data_transformer.rb
@@ -3,7 +3,9 @@ require 'json'
 module TimeZoneConverter
   class JsonDataTransformer
 
-    def initialize(path: 'data/cities')
+    DATA_PATH = 'data/cities'
+
+    def initialize(path: DATA_PATH)
       @path = path
     end
 
@@ -14,9 +16,45 @@ module TimeZoneConverter
         json = JSON.parse(File.read(json_file))
         json.each do |item|
           data = item.last
-          json_new[data['accentcity']] = [data['latitude'], data['longitude']]
+
+          next unless data.is_a? Hash
+
+          accentcity = data['accentcity']
+
+          if json_new[accentcity].present?
+            puts "#{accentcity} already exists."
+            population = data['population']
+            population_to_i = population.to_i || 0
+
+            previous_population_to_i = json_new[accentcity].try(:last).to_i || 0
+
+            if previous_population_to_i < population_to_i
+              puts "Overriding #{accentcity}."
+              puts "Population was #{previous_population_to_i}."
+              puts "Population is #{population_to_i}."
+              # sleep 0.1
+
+              json_new[accentcity] = [
+                data['latitude'],
+                data['longitude'],
+                population
+              ]
+            end
+          else
+            puts "#{accentcity} will be created."
+
+            json_new[accentcity] = [
+              data['latitude'],
+              data['longitude'],
+              data['population']
+            ]
+          end
+
+          # sleep 0.05
         end
       end
+
+      puts "Storing #{json_new.keys.count} cities."
 
       File.open("#{@path}/cities.json", "w") do |f|
         f.write(json_new.to_json)
